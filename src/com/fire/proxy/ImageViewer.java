6ad5cf627948eb8f63541658eec902431fb06444
jiefeng <jiefengfire@163.com>
2019-12-31 10:13:50 +0800

    wsl

---
+++ src/com/fire/proxy/ImageViewer.java
@@ -0,0 +1,10 @@
+public class ImageViewer {
+
+    public static void main(String[] args) throws Exception {
+        String image = "http://image.jpg";
+        URL url = new URL(image);
+        HighResolutionImage highResolutionImage = new HighResolutionImage(url);
+        ImageProxy imageProxy = new ImageProxy(highResolutionImage);
+        imageProxy.showImage();
+    }
+}
